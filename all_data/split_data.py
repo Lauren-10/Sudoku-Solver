053c5d8d0e3941b8a54bd2c8f2dfa2e1854f27a9
amandabarth <112357592+amandabarth@users.noreply.github.com>
2026-02-13 09:57:25 -0500

    split training data into two files so that github is happy with me again

--- all_data/split_data.py
+++ all_data/split_data.py
@@ -1,4 +1,5 @@
 import csv
+#Split testing and training data
 with open('test_sudoku.csv', 'w', newline='') as test_file:
     with open('train_sudoku.csv', 'w', newline='') as train_file:
         with open('sudoku.csv', 'r') as data_file:
@@ -11,3 +12,17 @@ with open('test_sudoku.csv', 'w', newline='') as test_file:
                     count += 1
                 else:
                     test.writerow(line)
+
+#Split training data into two files so github is happy
+with open("train_sudoku.csv", "r") as infile:
+    with open("train_sudoku1.csv", "w", newline='') as f1:
+        with open("train_sudoku2.csv", "w", newline='') as f2:
+            t1 = csv.writer(f1)
+            t2 = csv.writer(f2)
+            count = 0
+            for line in csv.reader(infile):
+                if count < 400000:
+                    t1.writerow(line)
+                    count += 1
+                else:
+                    t2.writerow(line)
